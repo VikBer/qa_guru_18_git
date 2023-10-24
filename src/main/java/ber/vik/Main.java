20c43ed87d90b6d4ea9a45b9998cf4e4728b2682
VikBer <111386131+VikBer@users.noreply.github.com>
2023-10-24 23:41:34 +0300

    Update Main.java
    more ! symbols

--- src/main/java/ber/vik/Main.java
+++ src/main/java/ber/vik/Main.java
@@ -2,6 +2,6 @@ package ber.vik;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Qa.guru!");
+        System.out.println("Hello Qa.guru!!!!!!!");
     }
 }
